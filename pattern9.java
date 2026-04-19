9d3751f451d35468bf5094af0a38c898f093c2f0
Nayeer Naushad <121220518+nayeer1169@users.noreply.github.com>
2026-04-19 19:01:37 +0530

    Add files via upload

---
+++ pattern9.java
@@ -0,0 +1,19 @@
+
+public class pattern9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n =5;     //if we change the number with 8
+		for (int i=1;i<=n;i++) {
+			for(int j=1;j<=n;j++) {
+//				if(i+j-1<10) {
+//					System.out.print(0);
+//				}
+				System.out.print(i+j-1+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
