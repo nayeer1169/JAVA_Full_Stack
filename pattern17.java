df77322fbb623df18f52ea036859b7124fbe1065
Nayeer Naushad <121220518+nayeer1169@users.noreply.github.com>
2026-04-20 17:59:00 +0530

    Add files via upload

---
+++ pattern17.java
@@ -0,0 +1,24 @@
+//12345
+//2  5
+//3 5
+//45
+//5
+
+public class pattern17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		for(int i =1;i<=n;i++) {
+			for(int j=i;j<=n;j++) {
+				if(j==n || i==1 || i==j) {
+					System.out.print(j+" ");
+				}else {
+					System.out.print("  ");
+				}
+			}System.out.println(" ");
+		}
+
+	}
+
+}
