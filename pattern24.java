b898fadad52c4a492d191323f6ad88369f64881a
Nayeer Naushad <121220518+nayeer1169@users.noreply.github.com>
2026-04-22 18:37:26 +0530

    Add files via upload

---
+++ pattern24.java
@@ -0,0 +1,30 @@
+//        1 
+//      1 2 1 
+//    1 2 3 2 1 
+//  1 2 3 4 3 2 1 
+//1 2 3 4 5 4 3 2 1 
+
+
+public class pattern24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			for(int k=1;k<=n-i;k++) {
+				System.out.print("  ");
+			}
+			int count=1;
+			for(int j=1;j<=2*i-1;j++) {
+				if(j<i) {
+					System.out.print(count++ +" ");
+				}else {
+					System.out.print(count-- +" ");
+				}
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
