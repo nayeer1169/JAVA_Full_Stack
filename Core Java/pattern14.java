396fba3f48dcb622d705495e292755c35d7c54a4
Nayeer Naushad <121220518+nayeer1169@users.noreply.github.com>
2026-05-01 18:16:52 +0530

    Add files via upload

---
+++ Core Java/pattern14.java
@@ -0,0 +1,24 @@
+
+public class pattern14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			for(int k=1;k<=n-i;k++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				if(j == 1 || i == 5 || i==j) {
+					System.out.print("* ");
+				}else {
+					System.out.print("  ");
+				}
+//				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
