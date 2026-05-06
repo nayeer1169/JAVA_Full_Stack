e83978cc4dd96791c3b4596e04c5974ed7cd8cb6
Nayeer Naushad <121220518+nayeer1169@users.noreply.github.com>
2026-05-06 17:47:42 +0530

    Add files via upload

---
+++ Core Java/pattern29.java
@@ -0,0 +1,25 @@
+//1 
+//3 2 
+//6 5 4 
+//10 9 8 7 
+//15 14 13 12 11 
+
+public class pattern29 {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		
+		
+		for(int i=1;i<=n;i++) {
+			int count=i*(i+1)/2;
+			for(int j=1;j<=i;j++) {
+				System.out.print(count+" ");
+				count--;
+			}
+			System.out.println();
+		}
+
+	}
+}
