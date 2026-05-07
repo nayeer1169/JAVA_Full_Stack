1173a1d8ab41d2e60a8ce9702a478a3a13c16ea5
Nayeer Naushad <121220518+nayeer1169@users.noreply.github.com>
2026-05-07 16:02:10 +0530

    Add files via upload

---
+++ Core Java/pattern31.java
@@ -0,0 +1,36 @@
+//A 
+//B B 
+//C C C 
+//D D D D 
+//E E E E E 
+
+public class pattern31 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+//		int ch=65;
+//		for(int i=1;i<=n;i++) {
+//			for(int j=1;j<=i;j++) {	
+//				System.out.print((char)ch + " ");
+//			}
+//			ch++;
+//			System.out.println();
+//		}
+		char ch='A';
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {	
+				System.out.print(ch + " ");
+			}
+			ch++;
+			System.out.println();
+		}
+
+	}
+
+}
+
+
+
+//here we can solve using both ways choice is yours.
+
