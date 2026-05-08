1b2ce27c9947a68d623a0d5ab6320f21b9c3774c
Nayeer Naushad <121220518+nayeer1169@users.noreply.github.com>
2026-05-08 18:42:55 +0530

    Add files via upload

---
+++ Core Java/pattern33.java
@@ -0,0 +1,27 @@
+//A 
+//B C 
+//D E F 
+//G H I J 
+//K L M N O 
+
+
+public class pattern33 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		char ch='A';
+		
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {	
+				
+				System.out.print(ch + " ");
+				ch++;
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
