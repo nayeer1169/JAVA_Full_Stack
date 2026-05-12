4e8bffdd48df2ca037b0642ed19405687971ab71
Nayeer Naushad <121220518+nayeer1169@users.noreply.github.com>
2026-05-12 19:06:07 +0530

    Add files via upload

---
+++ Programming/TwoSum.java
@@ -0,0 +1,17 @@
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		// INPUT
+		int a = 5;
+		int b = 10;
+		
+		// LOGIC
+		int sum = a+b;
+		
+		//OUTPUT
+		System.out.print("Sum = "+sum);
+
+	}
+
+}
