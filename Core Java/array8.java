24cd321c0c9e890defc459dd9eebf4f09ce8c01d
Nayeer Naushad <121220518+nayeer1169@users.noreply.github.com>
2026-05-13 17:55:07 +0530

    Add files via upload

--- Core Java/array8.java
+++ Core Java/array8.java
@@ -1,30 +1,33 @@
-//find the occurence of kth element
+//objects inside array - mobile details
 import java.util.Scanner;
-
 public class array8 {
-	public static int kOccurence(int[] a , int k){
-		int count = 0;
-		for(int i=0;i<a.length;i++) {
-			if(a[i]==k) {
-				count++;
-			}
-		}
-		return count;
-		
-		
-	}
+	
 
 	public static void main(String[] args) {
+		class MobileDetails{
+			String brand;
+			String model;
+			double price;
+			
+		}
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int a[] = new int[n];
+		MobileDetails m1 = null;
+		MobileDetails a[]  = new MobileDetails[2]; 
+		
 		for(int i=0;i<a.length;i++) {
-			a[i]=sc.nextInt();
+			m1 = new MobileDetails();
+			System.out.println("Enter the Brand of mobile:");
+			m1.brand=sc.next();
+			System.out.println("Enter the model of mobile:");
+			m1.model=sc.next();
+			System.out.println("Enter the price of mobile:");
+			m1.price = sc.nextDouble();	
+			a[i]=m1;
 		}
-		int k=sc.nextInt();
-		int res = kOccurence(a,k);
-		System.out.print(res);
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i].brand+" "+a[i].model+" "+a[i].price);
+		}
 
 	}
 
