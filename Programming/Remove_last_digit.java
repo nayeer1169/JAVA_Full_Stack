17022fd83252532ae8ecebb015e2cbf956909873
Nayeer Naushad <121220518+nayeer1169@users.noreply.github.com>
2026-05-14 13:31:19 +0530

    Add files via upload

---
+++ Programming/Remove_last_digit.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Remove_last_digit {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.print(n/10);
+		
+	}
+
+}
